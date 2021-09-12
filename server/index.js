1c06fc8f5bbb0a3b53f83209f7d2b0f1d3f2dba7
Alexander Klonov <alxender.klonov@mail.ru>
2021-09-12 12:50:17 +0300

    Finish server for hw1

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+const page404 = fs.readFileSync('./public/404.html')
+
+const server = http.createServer((req, res) => {
+    const path = req.url === '/' ? '/index.html' : req.url
+    console.log('request', req.url, path, req.headers['user-agent']);
+    
+    fs.readFile(`./public${path}`, (err, data) => {
+        if (err) {
+            data = page404
+        }
+        res.write(data);
+        res.end();
+    });
+});
+
+
+console.log("listening at http://127.0.0.1:8080")
+server.listen(8080)
