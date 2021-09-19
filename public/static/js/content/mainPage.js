6729228ef6657366973df45252e9ede1baea9203
Alexander Klonov <alxender.klonov@mail.ru>
2021-09-19 16:17:01 +0300

    Refactor js

---
+++ public/static/js/content/mainPage.js
@@ -0,0 +1,89 @@
+
+
+// добавляет поиск
+function loginPage() {
+
+    root.innerHTML = '';
+    
+    const content = document.createElement('div');
+    content.classList.add('content');
+
+    const productClass = document.createElement('div');
+    productClass.classList.add('product-path');
+    var productClassInfo = productPath('Volchock','Картины');
+    productClassInfo.forEach(item => {
+        productClass.appendChild(item);
+    })
+
+    content.appendChild(createNavigation('Одежда', 'Картины', 'Телефоны'));
+    content.appendChild(productClass);
+    content.appendChild(createProductGrid());
+    root.appendChild(content);
+    createFooter();
+}
+
+function productPath(category, subCategory) {
+    const cat = document.createElement('a');
+    cat.innerHTML = category;
+    cat.href="#";
+
+    const sign = document.createElement('p');
+    sign.innerHTML = '\u2192';
+
+    const subcat = document.createElement('a');
+    subcat.innerHTML = subCategory;
+    subcat.href="#";
+
+    const res = [cat, sign, subcat];
+    return res;
+}
+
+// Добавляет любое количество пунктов в выпадющее меню категорий и поиск
+function createNavigation(...categories) {
+    const navigation = document.createElement('div');
+    navigation.classList.add('navigation');
+
+    const nav = document.createElement('nav');
+    nav.classList.add('categories');
+    const menu = document.createElement('p');
+    menu.innerHTML = "III";
+    nav.appendChild(menu);
+
+    const input = document.createElement('input');
+    input.id = 'categories_toogle';
+    input.type = 'checkbox';
+    input.checked = true;
+    nav.appendChild(input);
+
+    // выпадающее меню с категориями
+    const catList = document.createElement('ul');
+    categories.forEach(elem => {
+        const cat = document.createElement('li');
+        const ref = document.createElement('a');
+        ref.href = '#';
+        ref.innerHTML = elem;
+        cat.appendChild(ref);
+        catList.appendChild(cat);
+    });
+    nav.appendChild(catList);
+
+
+    const srch = document.createElement('div');
+    srch.classList.add('search');
+
+    const srchInput = document.createElement('input');
+    srchInput.type = 'text';
+    srchInput.classList.add('searchTerm');
+    srchInput.placeholder = 'Поиск';
+    srch.appendChild(srchInput);
+
+    const srchBtn = document.createElement('button');
+    srchBtn.type = 'submit';
+    srchBtn.classList.add('searchButton');
+    srchBtn.innerHTML = "GO";
+    srch.appendChild(srchBtn);
+
+    navigation.appendChild(nav);
+    navigation.appendChild(srch);
+    return navigation;
+}
--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,124 +1,124 @@
-
-import {RENDER_METHODS, AJAX_STATUSES} from './constants.js'
 const wrapper = document.querySelector('.wrapper');
-const root = document.createElement('div');
-wrapper.appendChild(createHeader())
-root.id = 'root';
-wrapper.appendChild(root);
+const root = document.querySelector('#root');
+// wrapper.appendChild(createHeader())
+// root.id = 'root';
+// wrapper.appendChild(root);
 
-const configApp = {
-    menu: {
-        href: '/menu',
-        name: 'Меню',
-        open: menuPage,
-    },
-    profile: {
-        href: '/profile',
-        name: 'Профиль',
-        open: profilePage,
-    },
+// const configApp = {
+//     menu: {
+//         href: '/menu',
+//         name: 'Меню',
+//         open: menuPage,
+//     },
+//     profile: {
+//         href: '/profile',
+//         name: 'Профиль',
+//         open: profilePage,
+//     },
+// }
+
+// function createHeader() {
+//     let header = document.querySelector('.header');
+//     if (header) {
+//         wrapper.removeChild(header);
+//     }
+//     header = document.createElement('header');
+//     header.classList.add('header');
+//     const nav = document.createElement('nav');
+//     nav.classList.add('nav_menu');
+
+//     const brand = document.createElement('a');
+//     brand.href = '#';
+//     brand.innerHTML = 'Volchock';
+//     nav.appendChild(brand);
+
+//     const sign = document.createElement('p');
+//     sign.innerHTML = '|';
+//     nav.appendChild(sign);
+
+//     const newAd = document.createElement('a');
+//     newAd.href = '#';
+//     newAd.innerHTML = 'Создать объявление';
+//     nav.appendChild(newAd);
+
+//     const s = document.createElement('p');
+//     s.innerHTML = '|';
+//     nav.appendChild(s);
+
+//     ajax(
+//         'GET',
+//         '/profile',
+//         null,
+//         (status, responseText) => {
+//             let isAuthorized = false;
+    
+//             if (status === 200) {
+//                 isAuthorized = true;
+//             }
+//             if (!isAuthorized) {
+//                 const login = document.createElement('a');
+//                 login.href = '#';
+//                 login.innerHTML = 'Войти';
+//                 login.id = 'auth';
+//                 login.addEventListener('click', e => {
+//                     e.preventDefault();
+                    
+//                     const window = document.querySelector('.modal-window');
+//                     const blackout = document.querySelector('.blackout');
+                
+//                     createLoginForm(window);
+                
+//                     window.classList.add('active');
+//                     blackout.classList.add('active');
+//                 })
+//                 nav.appendChild(login);
+//             }
+//             else {
+//                 const profile = document.createElement('li');
+//                 profile.classList.add('dropdown');
+//                 profile.innerHTML = 'Профиль';
+
+//                 const menu = document.createElement('ul');
+//                 menu.classList.add('dropdown_menu');
+//                 menu.classList.add('dropdown_menu--animated');
+
+//                 const content = document.createElement('li');
+//                 content.classList.add("dropdown_item");
+
+//                 const imgref = document.createElement('a');
+//                 imgref.href='#';
+
+//                 const img = document.createElement('img');
+//                 img.src = 'https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg';
+//                 imgref.appendChild(img);
+//                 content.appendChild(imgref);
+
+//                 const logout = document.createElement('li');
+//                 logout.classList.add("logout_item");
+
+//                 const logoutBtn = document.createElement('a');
+//                 logoutBtn.href = '/logout';
+//                 logoutBtn.innerHTML = 'Выход';
+
+//                 logout.appendChild(logoutBtn);
+
+//                 menu.appendChild(content);
+//                 menu.appendChild(logout);
+//                 profile.appendChild(menu);
+//                 nav.appendChild(profile);
+//             }
+//         });
+//     header.appendChild(nav);
+//     return header;
+// }
+
+function createFooter() {
+    const footer = Handlebars.compile("<footer class = 'footer' <p>{{footerText}} </p>");
+    wrapper.insertAdjacentHTML("beforeend", footer({ footerText: "© 2021. Volchock team"}));
 }
 
 function createHeader() {
-    let header = document.querySelector('.header');
-    if (header) {
-        wrapper.removeChild(header);
-    }
-    header = document.createElement('header');
-    header.classList.add('header');
-    const nav = document.createElement('nav');
-    nav.classList.add('nav_menu');
-
-    const brand = document.createElement('a');
-    brand.href = '#';
-    brand.innerHTML = 'Volchock';
-    nav.appendChild(brand);
-
-    const sign = document.createElement('p');
-    sign.innerHTML = '|';
-    nav.appendChild(sign);
-
-    const newAd = document.createElement('a');
-    newAd.href = '#';
-    newAd.innerHTML = 'Создать объявление';
-    nav.appendChild(newAd);
-
-    const s = document.createElement('p');
-    s.innerHTML = '|';
-    nav.appendChild(s);
-
-    ajax(
-        'GET',
-        '/profile',
-        null,
-        (status, responseText) => {
-            let isAuthorized = false;
-    
-            if (status === 200) {
-                isAuthorized = true;
-            }
-            if (!isAuthorized) {
-                const login = document.createElement('a');
-                login.href = '#';
-                login.innerHTML = 'Войти';
-                login.id = 'auth';
-                login.addEventListener('click', e => {
-                    e.preventDefault();
-                    
-                    const window = document.querySelector('.modal-window');
-                    const blackout = document.querySelector('.blackout');
-                
-                    createLoginForm(window);
-                
-                    window.classList.add('active');
-                    blackout.classList.add('active');
-                })
-                nav.appendChild(login);
-            }
-            else {
-                const profile = document.createElement('li');
-                profile.classList.add('dropdown');
-                profile.innerHTML = 'Профиль';
-
-                const menu = document.createElement('ul');
-                menu.classList.add('dropdown_menu');
-                menu.classList.add('dropdown_menu--animated');
-
-                const content = document.createElement('li');
-                content.classList.add("dropdown_item");
-
-                const imgref = document.createElement('a');
-                imgref.href='#';
-
-                const img = document.createElement('img');
-                img.src = 'https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg';
-                imgref.appendChild(img);
-                content.appendChild(imgref);
-
-                const logout = document.createElement('li');
-                logout.classList.add("logout_item");
-
-                const logoutBtn = document.createElement('a');
-                logoutBtn.href = '/logout';
-                logoutBtn.innerHTML = 'Выход';
-
-                logout.appendChild(logoutBtn);
-
-                menu.appendChild(content);
-                menu.appendChild(logout);
-                profile.appendChild(menu);
-                nav.appendChild(profile);
-            }
-        });
-    header.appendChild(nav);
-    return header;
+    const header = Handlebars.compile("")
 }
 
-function createFooter() {
-    const footer = document.createElement('footer');
-    footer.classList.add('footer');
-    const footerText = document.createElement('p');
-    footerText.innerHTML = '\u00A9 2021. Volchock team';
-    footer.appendChild(footerText);
-    wrapper.appendChild(footer);
-}
+createFooter();
