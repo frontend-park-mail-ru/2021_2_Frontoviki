c36e767fdb618ca175034df526175d7cb2ef2161
Ekasy <mikkycat15@gmail.com>
2021-09-22 00:56:29 +0300

    added validation and promts

--- public/static/js/content/modal.js
+++ public/static/js/content/modal.js
@@ -28,30 +28,68 @@ export function CreateModal() {
     const logForm = document.createElement('form');
     logForm.classList.add('modal-form');
 
+    /* Инпут емайла с блоком подсказок */
     const logemail = document.createElement('input');
     logemail.type = 'email';
     logemail.name = 'email';
     logemail.placeholder = 'Email';
+    logemail.addEventListener('keyup', (e) => {
+        e.target.classList = '';
+    });
     logForm.appendChild(logemail);
 
-    const promtEmail = document.createElement('p');
-    promtEmail.classList.add('promt');
-    promtEmail.innerHTML = 'Емайл должен быть валидным';
-    logForm.appendChild(promtEmail);
+    const promtEmailBlockLogin = document.createElement('div');
+    promtEmailBlockLogin.name = 'signup-username';
+    promtEmailBlockLogin.classList.add('promt-block');
 
+    const promtEmailInvalidLogin = document.createElement('p');
+    promtEmailInvalidLogin.classList.add('promt');
+    promtEmailInvalidLogin.innerHTML = 'Неверный емайл';
+    promtEmailBlockLogin.appendChild(promtEmailInvalidLogin);
+    logForm.appendChild(promtEmailBlockLogin);
+
+    /* Инпут пароля с блоком подсказок */
     const logpassword = document.createElement('input');
     logpassword.type = 'password';
     logpassword.name = 'password';
     logpassword.placeholder = 'Пароль';
+    logpassword.addEventListener('keyup', (e) => {
+        e.target.classList = '';
+    });
     logForm.appendChild(logpassword);
 
-    const promtPassword = document.createElement('p');
-    promtPassword.classList.add('promt');
-    promtPassword.innerHTML = 'Пароль должен состоять из букв, цифр и спец символов, длинной от 8 символов';
-    logForm.appendChild(promtPassword);
+    const promtPasswordBlockLogin = document.createElement('div');
+    promtPasswordBlockLogin.name = 'signup-username';
+    promtPasswordBlockLogin.classList.add('promt-block');
+
+    const promtPasswordInvalidLogin = document.createElement('p');
+    promtPasswordInvalidLogin.classList.add('promt');
+    promtPasswordInvalidLogin.innerHTML = 'Неверный пароль';
+    promtPasswordBlockLogin.appendChild(promtPasswordInvalidLogin);
+    logForm.appendChild(promtPasswordBlockLogin);
+    /* конец блока подсказок логина */
 
     const btn = document.createElement('input');
     btn.type = 'submit';
+
+    const patterns = {
+        username: /^[a-z\d]{5,20}$/i,
+        email: /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/,
+        password: /^[a-z\d]{5,20}$/i, ///^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}$/i,
+        telephone: /^\d{11}$/,
+        slug: /^[a-z\d-]{8,20}$/i,
+    };
+
+    const validate = (field, regex) => {
+        const valid = regex.test(field.value);
+        if (valid) {
+            field.className = 'valid';
+        }
+        else {
+            field.className = 'invalid';
+        }
+        return valid;
+    };
 
     logForm.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -59,8 +97,8 @@ export function CreateModal() {
         const email = logemail.value.trim();
         const password = logpassword.value.trim();
         Ajax.ajaxPost({
-            url : '/login',
-            body : {email, password},
+            url: '/login',
+            body: { email, password },
             callback: (status) => {
                 if (status === 200) {
                     // в случае если мы зашли убрать модальное и обновить хедер
@@ -68,8 +106,12 @@ export function CreateModal() {
                     black.click();
                     return;
                 }
+                // например неверный пароль
+                if (true) {
+                    logpassword.className = 'invalid';
+                }
                 alert('НЕ получилось не фартануло');
-              }
+            }
         });
     });
 
@@ -96,29 +138,102 @@ export function CreateModal() {
     const regForm = document.createElement('form');
     regForm.classList.add('modal-form');
 
+    /* Инпут имени пользователя с блоком подсказок */
     const nameR = document.createElement('input');
-    nameR.type = 'username';
-    nameR.type = 'username';
+    nameR.type = 'text';
+    nameR.name = 'username';
     nameR.placeholder = 'Имя';
+    nameR.addEventListener('keyup', (e) => {
+        e.target.classList = '';
+        // очищаю класс show у server-invalid
+        const promt = e.target.nextSibling.firstChild;  // input->div->p
+        promt.classList.remove('show');
+    });
     regForm.appendChild(nameR);
 
+    const promtUsernameBlock = document.createElement('div');
+    promtUsernameBlock.name = 'signup-username';
+    promtUsernameBlock.classList.add('promt-block');
+
+    const promtUsernameAlrdyExist = document.createElement('p');
+    promtUsernameAlrdyExist.classList.add('promt', 'server-invalid');
+    promtUsernameAlrdyExist.innerHTML = 'Пользователь с таким именем уже существует';
+    promtUsernameBlock.appendChild(promtUsernameAlrdyExist);
+
+    const promtUsernameInvalidName = document.createElement('p');
+    promtUsernameInvalidName.classList.add('promt');
+    promtUsernameInvalidName.innerHTML = 'Имя пользователя должно быть от 5 до 20 символов';
+    promtUsernameBlock.appendChild(promtUsernameInvalidName);
+    regForm.appendChild(promtUsernameBlock);
+
+    /* Инпут имейла с блоком подсказок */
     const emailR = document.createElement('input');
     emailR.type = 'email';
     emailR.name = 'email';
     emailR.placeholder = 'Email';
+    emailR.addEventListener('keyup', (e) => {
+        e.target.classList = '';
+        // очищаю класс show у server-invalid
+        const promt = e.target.nextSibling.firstChild;  // input->div->p
+        promt.classList.remove('show');
+    });
     regForm.appendChild(emailR);
 
+    const promtEmailBlock = document.createElement('div');
+    promtEmailBlock.classList.add('promt-block');
+    promtEmailBlock.name = 'signup-email';
+
+    const promtEmailAlrdyExist = document.createElement('p');
+    promtEmailAlrdyExist.classList.add('promt', 'server-invalid');
+    promtEmailAlrdyExist.innerHTML = 'Этот емайл уже используется';
+    promtEmailBlock.appendChild(promtEmailAlrdyExist);
+
+    const promtEmailInvalidName = document.createElement('p');
+    promtEmailInvalidName.classList.add('promt');
+    promtEmailInvalidName.innerHTML = 'Неверный формат емайла (пример: exam@ple.com)';
+    promtEmailBlock.appendChild(promtEmailInvalidName);
+    regForm.appendChild(promtEmailBlock);
+
+    /* Инпут пароля с блоком подсказок */
     const passwordR = document.createElement('input');
     passwordR.type = 'password';
     passwordR.name = 'password';
     passwordR.placeholder = 'Пароль';
+    passwordR.addEventListener('keyup', (e) => {
+        e.target.classList = '';
+        // очищаю класс show у server-invalid
+        const promt = e.target.nextSibling.firstChild;  // input->div->p
+        promt.classList.remove('show');
+    });
     regForm.appendChild(passwordR);
 
+    const promtPasswordBlock = document.createElement('div');
+    promtPasswordBlock.classList.add('promt-block');
+    promtPasswordBlock.name = 'signup-password';
+
+    const promtPasswordInvalid = document.createElement('p');
+    promtPasswordInvalid.classList.add('promt');
+    promtPasswordInvalid.innerHTML = 'Пароль должен состоять из букв, цифр и спец символов, длинной от 8 символов';
+    promtPasswordBlock.appendChild(promtPasswordInvalid);
+    regForm.appendChild(promtPasswordBlock);
+
+    /* Инпут повтора пароля с блоком подсказок */
     const passwordRepR = document.createElement('input');
     passwordRepR.type = 'password';
     passwordRepR.name = 'password';
     passwordRepR.placeholder = 'Повторите Пароль';
     regForm.appendChild(passwordRepR);
+
+    const promtRepPasswordBlock = document.createElement('div');
+    promtRepPasswordBlock.classList.add('promt-block');
+    promtRepPasswordBlock.name = 'signup-password';
+
+    const promtRepPasswordInvalid = document.createElement('p');
+    promtRepPasswordInvalid.classList.add('promt');
+    promtRepPasswordInvalid.innerHTML = 'Пароли должны совпадать';
+    promtRepPasswordBlock.appendChild(promtRepPasswordInvalid);
+    regForm.appendChild(promtRepPasswordBlock);
+    /* конец блока подсказок */
 
     const btnR = document.createElement('input');
     btnR.type = 'submit';
@@ -134,12 +249,26 @@ export function CreateModal() {
     regForm.addEventListener('submit', (e) => {
         e.preventDefault();
 
+        let valid = validate(nameR, patterns[nameR.attributes.name.value]) &&
+                    validate(emailR, patterns[emailR.attributes.name.value]) &&
+                    validate(passwordR, patterns[passwordR.attributes.name.value]);
+
         const name = nameR.value.trim();
         const email = emailR.value.trim();
-        const password = passwordR.value.trim();;
+        const password = passwordR.value.trim();
+        const passwordRep = passwordRepR.value.trim();
+
+        if (password !== passwordRep) {
+            passwordRepR.className = 'invalid';
+        }
+
+        if (!valid || password !== passwordRep) {
+            return;
+        }
+
         Ajax.ajaxPost({
-            url : '/signup',
-            body : {email, password, name},
+            url: '/signup',
+            body: { email, password, name },
             callback: (status) => {
                 if (status === 201) {
                     // если зарегались, показываем окно логина
@@ -147,7 +276,13 @@ export function CreateModal() {
                     return;
                 }
                 alert('НЕ получилось не фартануло');
-              }
+                // типа ошибка в том, что пользователь существует
+                if (true) {
+                    console.log('exist');
+                    promtUsernameAlrdyExist.classList.add('show');
+                    nameR.className = 'invalid';
+                }
+            }
         });
     });
 
