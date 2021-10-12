3a148277497837f9eed454b879315bfc4a82d27e
Alexander Klonov <alxender.klonov@mail.ru>
2021-10-13 00:00:52 +0300

    Solved all rk1 issues

---
+++ public/static/js/content/templates/input/input.js
@@ -0,0 +1,8 @@
+
+export function createInput(title, description) {
+    const inputTemplate = Handlebars.templates.input;
+    const input = document.createElement('div');
+    input.innerHTML = inputTemplate({inputName: title, inputDesc: description});
+    input.classList.add('text-input');
+    return input;
+}
---
+++ public/static/js/modules/isLogged.js
@@ -0,0 +1,15 @@
+import {secureDomainUrl} from '../constatns.js';
+import {Ajax} from './ajax.js';
+
+/**
+ * функция отправки запроса на сервер, чтобы проверить
+ * вошел ли пользователь в аккаунт
+ * @return {Promise} ответ с сервера
+ */
+export function isLogged() {
+  const res = Ajax.asyncGetUsingFetch({
+    url: secureDomainUrl + 'users/profile',
+    body: null,
+  });
+  return res;
+}
