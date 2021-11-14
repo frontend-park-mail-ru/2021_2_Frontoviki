175c22d786743287b3c3c7b9d7e5cdea0e52b89c
Ekasy <mikkycat15@gmail.com>
2021-11-14 18:56:26 +0300

    new design to upload images

--- newAdvPage/newAdv.js
+++ newAdvPage/newAdv.js
@@ -14,3 +14,20 @@ input.forEach(elem => {
         }
     });
 });
+
+// ДОБАВЛЕНИЕ НОВОГО КУБИКА В ЗАГРУЗЧИК КАРТИНОК
+
+const fileInput = document.getElementById('image_upload');
+const imageUploader = document.getElementById('image-uploader');
+
+const insertImageIntoImageUploader = url => {
+  // тут создаешь кубик и вставляешь его 
+  // на предпоследнее место перед label
+}
+
+fileInput.onchange = e => {
+  const [file] = fileInput.files;
+  if (file) {
+    insertImageIntoImageUploader(URL.createObjectURL(file));
+  }
+}
